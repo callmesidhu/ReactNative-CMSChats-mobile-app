7592dbb3a250ed4b0b773c0b9e7d1c354ba1d034
S Sidharth <sidharthpunalur@gmail.com>
2024-06-01 13:03:34 +0530

    login page animation added

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import SignUp from './Pages/SignUp';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+
 export default function App() {
 
   return (
+    
       <NavigationContainer>
       <Stack.Navigator initialRouteName='LoginPage' screenOptions={{headerShown: false}}>
         <Stack.Screen name="LoginPage" component={Login} />
         <Stack.Screen name="SignUpPage" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
+  
   );
 }
 
--- Pages/Login.js
+++ Pages/Login.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import { Image, View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
+import Animated,{ FadeInUp, FlipInXDown, FlipInXUp, StretchInY } from 'react-native-reanimated';
 
 const Login = () => {
   return (
     <View className='flex-1'>
 
       <StatusBar style='light'/>
-      <Image className='flex-1 w-full h-[120%] absolute' source={require('../Resources/background.png')}  resizeMode="cover"/>
+      <Animated.Image entering={FadeInUp.delay(100).duration(1000)} className='flex-1 w-full h-[120%] absolute' source={require('../Resources/background.png')}  resizeMode="cover"/>
       <View className='flex-row justify-around absolute w-full'>
-        <Image className='h-[225] w-[90]' source={require('../Resources/light.png')}/> 
-        <Image className='h-[160] w-[65] ' source={require('../Resources/light.png')}/>
+        <Animated.Image entering={FadeInUp.delay(800).duration(2000).springify()} className='h-[225] w-[90]' source={require('../Resources/light.png')}/> 
+        <Animated.Image entering={FadeInUp.delay(1000).duration(2000).springify()} className='h-[160] w-[65] ' source={require('../Resources/light.png')}/>
       </View>
 
       <View className='h-full w-full flex justify-center items-center'>
 
-       <View className='flex-1 mt-60 justify-center '>
-            <Text className='text-white font-bold tranking-wider text-5xl'>
+       <View className='flex-1 mt-56 justify-center'>
+            <Animated.Text className='text-white font-bold tranking-wider text-5xl'>
               Login
-            </Text>
+            </Animated.Text>
         </View>
 
        <View className='flex-1 item-center space-y-4 w-full px-4'>
-            <View className='bg-black/5 p-4 rounded-2xl w-full'>
+            <Animated.View className='bg-black/5 p-4 rounded-2xl w-full'>
               <TextInput placeholder='Email' placeholderTextColor={'gray'}/>
-            </View>
-            <View className='bg-black/5 p-4 rounded-2xl w-full'>
+            </Animated.View>
+            <Animated.View  className='bg-black/5 p-4 rounded-2xl w-full'>
               <TextInput placeholder='Password' placeholderTextColor={'gray'} secureTextEntry/>
-            </View>
-            <View className='w-full pt-8'>
+            </Animated.View>
+            <Animated.View entering={StretchInY.delay(800).duration(500)} className='w-full pt-8'>
               <TouchableOpacity className='bg-sky-400 p-3 rounded-2xl mb-3 items-center'>
                 <Text className='text-xl font-bold text-white'>Login</Text>
               </TouchableOpacity>
-            </View>
+            </Animated.View>
             <View className='justify-center flex-row'>
               <Text>Don't have a account?</Text>
               <TouchableOpacity>
