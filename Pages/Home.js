637c6fdcd1efc6486f204bb70fac61c09cec35e1
S Sidharth <sidharthpunalur@gmail.com>
2024-06-22 11:45:35 +0530

    Home page Checkpoint

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
-export default function Home() {
-  return (
-    <View className='flex-1 p-28 justify-center'>
-      <Text className='text-center'>Welcome to Home Page </Text>
-    </View>
-  )
-}
+
+  export default function Home() {
+    return (
+          <View className='flex-1'>
+            <View className='bg-sky-600 rounded-b-3xl h-24 shadow-2xl shadow-black pt-12 px-5'>
+              <Text className='text-2xl text-white font-medium'>Chats</Text>
+              <View>
+                
+              </View>
+            </View>
+          </View>
+    )
+  }
--- Pages/PageLoader.js
+++ Pages/PageLoader.js
@@ -1,28 +1,27 @@
-import { Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 
 export default function PageLoader() {
   const navigation = useNavigation();
 
-  var isAuthenticated=false;
+  var isAuthenticated = true;
 
   useEffect(() => {
-    setTimeout(()=>{
-      if(!isAuthenticated){
+    setTimeout(() => {
+      if (!isAuthenticated) {
         navigation.navigate("LoginPage");
-      }else{
+      } else {
         navigation.navigate("HomePage");
       }
-    },5000);
-  }, [])
-
+    }, 2000);
+  }, []);
 
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      
-      <Text>Please Wait...</Text>
+    <View className='flex-1 justify-center items-center'>
+      <ActivityIndicator size="large" color="#00FFFF" />
+      <Text>Please wait...</Text>
     </View>
-  )
-}
+  );
+};
